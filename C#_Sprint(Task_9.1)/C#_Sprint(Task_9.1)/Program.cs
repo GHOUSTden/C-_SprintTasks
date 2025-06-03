9b2d15f3aa983c3a40210e7a0cf5078b458f0c23
GHOUSTden <ghoust.den2007@gmail.com>
2025-06-03 18:44:55 +0300

    fourth commit

---
+++ C#_Sprint(Task_9.1)/C#_Sprint(Task_9.1)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Sprint_Task_9._1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть кількість: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
