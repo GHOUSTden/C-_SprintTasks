f9468938cb97f54a4ea5145c409de930bb16204b
GHOUSTden <ghoust.den2007@gmail.com>
2025-06-03 18:41:46 +0300

    third commit

---
+++ C#_Sprint(Task_7.1)/C#_Sprint(Task_7.1)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Sprint_Task_7._1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть перше число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введіть друге число: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Розрахунки: ");
+
+            Console.WriteLine($"{a} + {b} = {a + b}");
+            Console.WriteLine($"{a} - {b} = {a - b}");
+            Console.WriteLine($"{a} * {b} = {a * b}");
+            Console.WriteLine($"{a} / {b} = {a / b}");
+        }
+    }
+}
