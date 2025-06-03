8965c69c440c7e72fdfd5b9f4731b18dc30285d9
GHOUSTden <ghoust.den2007@gmail.com>
2025-06-03 18:35:37 +0300

    first commit

---
+++ C#_Sprint(Task_4.1)/C#_Sprint(Task_4.1)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Sprint_Task_4._1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Danya");
+            Console.WriteLine("I'm from Ukraine");
+        }
+    }
+}
