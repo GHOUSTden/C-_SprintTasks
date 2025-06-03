d0868e1d07a6dd56a39c5014f7b7d0dc650133a6
GHOUSTden <ghoust.den2007@gmail.com>
2025-06-03 18:38:34 +0300

    second commit

---
+++ C#_Sprint(Task_6.1)/C#_Sprint(Task_6.1)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Sprint_Task_6._1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть свій вік: ");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("Користувач повнолітній");
+            }
+            else
+            {
+                Console.WriteLine("Користувач неповнолітній");
+            }
+        }
+    }
+}
